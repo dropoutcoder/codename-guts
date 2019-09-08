deba639d9d7326e67c771f51a77884e2eeb3917f
Petr Šrámek <me@dropoutcoder.com>
2019-09-08 19:43:22 +0200

    Added Check class

---
+++ src/DropoutCoder.Core/Check.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropoutCoder.Core {
+    public abstract partial class Check {
+        private Check() { }
+    }
+}
