44ff022bf8c9c35478ff29c754b50beeae7881d3
Petr Šrámek <me@dropoutcoder.com>
2019-09-09 15:11:57 +0200

    Added Is<T> type comparison method

--- src/DropoutCoder.Core/Check.cs
+++ src/DropoutCoder.Core/Check.cs
@@ -9,5 +9,15 @@ namespace DropoutCoder.Core {
     /// </summary>
     public abstract partial class Check {
         private Check() { }
+
+        /// <summary>
+        /// Indicates whether <paramref name="value"/> equals to type of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to compare.</typeparam>
+        /// <param name="value">An object to compare with <typeparamref name="T"/></param>
+        /// <returns>true if <paramref name="value"/> type if of type <typeparamref name="T"/>; otherwise, false.</returns>
+        public static bool Is<T>(object value) {
+            return value is T;
+        }
     }
 }
