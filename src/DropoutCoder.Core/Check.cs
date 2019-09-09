dd49e8da6dc72236aab485d4d00d052e82f41964
Petr Šrámek <me@dropoutcoder.com>
2019-09-09 15:17:50 +0200

    Updated comment for Check class and removed usings

--- src/DropoutCoder.Core/Check.cs
+++ src/DropoutCoder.Core/Check.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace DropoutCoder.Core {
     /// <summary>
-    /// Provides methods for type and value validation
+    /// Provides methods for common type and value validation
     /// </summary>
     public abstract partial class Check {
         private Check() { }
