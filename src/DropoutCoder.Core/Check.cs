c54f04d24534c92cc4a971b6d52eeab1621dac59
Petr Šrámek <me@dropoutcoder.com>
2019-09-09 15:10:47 +0200

    Added missing curly brace

--- src/DropoutCoder.Core/Check.cs
+++ src/DropoutCoder.Core/Check.cs
@@ -4,9 +4,10 @@ using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DropoutCoder.Core {
-       /// <summary>
-       /// Provides methods for type and value validation
-       /// </summary>
+    /// <summary>
+    /// Provides methods for type and value validation
+    /// </summary>
     public abstract partial class Check {
         private Check() { }
+    }
 }
