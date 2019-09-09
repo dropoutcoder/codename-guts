281696dbc2b048db628dbb9311ac27e03e6ca38e
Petr Šrámek <me@dropoutcoder.com>
2019-09-09 16:17:43 +0200

    Added default object overrides to Thwo class

--- src/DropoutCoder.Core/Throw.cs
+++ src/DropoutCoder.Core/Throw.cs
@@ -1,8 +1,26 @@
-﻿namespace DropoutCoder.Core {
+﻿using System.ComponentModel;
+
+namespace DropoutCoder.Core {
     /// <summary>
     /// Provides methods for common validation and throwing exception if validation didn't pass
     /// </summary>
     public abstract partial class Throw {
         private Throw() { }
+
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) {
+            return base.Equals(obj);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override string ToString() {
+            return base.ToString();
+        }
     }
 }
