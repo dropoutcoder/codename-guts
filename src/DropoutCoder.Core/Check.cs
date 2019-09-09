d91ce3085987f60957453b5e64f10e55bd710311
Petr Šrámek <me@dropoutcoder.com>
2019-09-09 15:10:24 +0200

    Added Check class

--- src/DropoutCoder.Core/Check.cs
+++ src/DropoutCoder.Core/Check.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DropoutCoder.Core {
+       /// <summary>
+       /// Provides methods for type and value validation
+       /// </summary>
     public abstract partial class Check {
         private Check() { }
-    }
 }
